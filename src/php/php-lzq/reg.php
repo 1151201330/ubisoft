77fa17e8ede8d4739c769d6d1076d85e16e14b2e
lzq <1360408911@qq.com>
2018-10-08 08:55:08 +0800

    2018-10-08

--- src/php/php-lzq/reg.php
+++ src/php/php-lzq/reg.php
@@ -4,4 +4,21 @@
   mysql_select_db("ubisoft",$db); //选择数据库 数据库名称 连接
   mysql_query("set names utf8");//设置编码格式
 
+
+  $email = $_REQUEST["email"];
+  $upwd = $_REQUEST["password"];
+
+  $sql2 = "SELECT * FROM `ubisolt_register` WHERE (`reg_email`='$email')";  //查询前台传入的用户名
+  $data = mysql_query($sql2);
+
+ $result = mysql_fetch_array($data); //将查询结果提取成数组
+ if($result){
+     echo '{"status":200,"msg":"用户名已存在","res":false}';
+ }else{
+     $sql3 = "INSERT INTO `baidu_user`(`u_id`, `u_username`, `u_password`, `u_sex`) VALUES (NULL,'$uname','$upwd',0)";
+     $count = mysql_query($sql3); // 增加 删除 修改 返回受影响的行数
+     if($count==1){
+         echo '{"status":200,"msg":"注册成功","res":true}';
+     }
+ }
 ?>
