8cf84d16f634e19dbeafd46bd8c306e350fb0aa7
lzq <1360408911@qq.com>
2018-10-12 15:20:45 +0800

    2018-10-12

--- src/js/js-lzq/news-more.js
+++ src/js/js-lzq/news-more.js
@@ -2,19 +2,29 @@ $(function() {
     // $('.top').load('http://10.31.157.13:8080/project-lzq/ubisoft/src/html/head-ha.html');
     $(document).on('click', function(ev) {
         if (ev.target == $('.platform')[0]) {
-            $('.drop').addClass('show');
+            $('.drop').toggleClass('show');
         } else {
             $('.drop').removeClass('show');
         }
     })
-
     $(document).on('click', function(ev) {
         if (ev.target == $('.game')[0]) {
-            $('.drop2').addClass('show');
+            $('.drop2').toggleClass('show');
         } else {
             $('.drop2').removeClass('show');
         }
     })
+    $('.drop li a').on('click', function() {
+        $('.drop li a').removeClass('active');
+        $('#all').html($(this).html());
+        $(this).addClass('active');
+    })
+    $('.drop2 li a').on('click', function() {
+        $('.drop2 li a').removeClass('active');
+        $('#all2').html($(this).html());
+        $(this).addClass('active');
+    })
+
     $('.btm').load('../html/foot-ha.html');
 
 })
